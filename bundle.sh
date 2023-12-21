c435746f855737e071c58d6372399e54b5762c33
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-12-20 22:19:14 -0600

    Make the bundler more efficient and the payload smaller

--- bundle.sh
+++ bundle.sh
@@ -14,6 +14,7 @@ PATHS=(
   "bootstrap/"
   "config/"
   "database/"
+  "nginx/"
   "public/"
   "resources/"
   "routes/"
@@ -24,34 +25,67 @@ PATHS=(
   "storage/logs/.gitignore"
   "artisan"
   "composer.json"
-  "install.sh"
   "update.sh"
+  ".env.example"
 )
 
 # Add Composer production dependencies to the PATHS list.
 # This is done as a build time optimization to minimize the payload.
-IGNORED_PATHS=(
+IGNORED_FILENAMES=(
     "phpunit.xml"
     ".editorconfig"
+    "CONTRIBUTING.md"
+    "SECURITY.md"
+    "CHANGELOG.md"
+    "CHANGES.txt"
+    "README.md"
+    "readme.md"
+    "UPGRADE.md"
+    "INFO.md"
+    "composer.lock"
+    ".phpstorm.meta.php"
+    ".readthedocs.yml"
+    "docker-compose.yml"
+    "conventional-commits.json"
+)
+IGNORED_PATHS=(
+    "vendor/doctrine/inflector/docs"
+    "vendor/psr/http-message/docs"
 )
 IGNORED_FILES=()
 COMPOSER_PATHS=$(composer show --path --no-dev | awk '{print $2}' | sed "s|$DIR/||" | sed "s|$|/|")
 for COMPOSER_PATH in $COMPOSER_PATHS; do
   FILES=$(ls -A "$COMPOSER_PATH")
   for FILE in $FILES; do
-    # If the file name ends with ".md" and does not start with "LICEN" or "licen", skip it.
-    if [[ "$FILE" =~ \.md$ ]] && ! [[ "$FILE" =~ ^[Ll][Ii][Cc][Ee][Nn] ]]; then
+    if [[ " ${IGNORED_FILENAMES[@]} " =~ " $FILE " ]]; then
       IGNORED_FILES+=("$COMPOSER_PATH$FILE")
       continue
-    fi
-    # If the file name is in the IGNORED_PATHS list, skip it.
-    if [[ " ${IGNORED_PATHS[@]} " =~ " $FILE " ]]; then
+    elif [[ "$COMPOSER_PATH$FILE" == *"vendor/"*".github/"* ]]; then
       IGNORED_FILES+=("$COMPOSER_PATH$FILE")
       continue
+    elif [[ "$COMPOSER_PATH$FILE" == *"vendor/"*".gitignore" ]]; then
+      IGNORED_FILES+=("$COMPOSER_PATH$FILE")
+      continue
+    elif [[ "$COMPOSER_PATH$FILE" == *"vendor/"*".gitkeep" ]]; then
+      IGNORED_FILES+=("$COMPOSER_PATH$FILE")
+      continue
+    elif [[ "$COMPOSER_PATH$FILE" == *"vendor/"*".favicon" ]]; then
+      IGNORED_FILES+=("$COMPOSER_PATH$FILE")
+      continue
+    else
+      for IGNORED_PATH in "${IGNORED_PATHS[@]}"; do
+        if [[ "$COMPOSER_PATH$FILE" == *"$IGNORED_PATH"* ]]; then
+          IGNORED_FILES+=("$COMPOSER_PATH$FILE")
+          continue 2
+        fi
+      done
     fi
     PATHS+=("$COMPOSER_PATH$FILE")
   done
 done
+
+# Create the autoload file.
+composer dump-autoload --no-dev --optimize --classmap-authoritative --no-interaction --no-scripts --no-plugins
 
 # Zip paths and output a log to a file.
 zip -r -D -l -1 -v app.zip ${PATHS[@]} > "$LOGFILE.tmp" 2>&1
@@ -63,7 +97,7 @@ echo "Runtime: $RUNTIME seconds"
 RESULT=$(tail -n 1 "$LOGFILE.tmp")
 
 echo "================================" > "$LOGFILE"
-echo "= Application Package Manifest =" >> "$LOGFILE"
+echo "APPLICATION MANIFEST ===========" >> "$LOGFILE"
 echo "================================" >> "$LOGFILE"
 echo "> Runtime: $RUNTIME seconds" >> "$LOGFILE"
 echo "> Results: $RESULT" >> "$LOGFILE"
@@ -72,8 +106,11 @@ for IGNORED_FILE in "${IGNORED_FILES[@]}"; do
   echo "  - $IGNORED_FILE" >> "$LOGFILE"
 done
 echo "================================" >> "$LOGFILE"
+echo "CONTENTS =======================" >> "$LOGFILE"
+echo "================================" >> "$LOGFILE"
+
+# Add the contents of the zip to the log file.
 cat "$LOGFILE.tmp" >> "$LOGFILE"
-rm "$LOGFILE.tmp"
 
 # Add the log file to the zip.
 zip -u -1 app.zip "$LOGFILE" > /dev/null 2>&1
