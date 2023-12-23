415d6d54ecc79c7f598d13d94960236bc19e58f8
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-12-23 14:54:45 -0600

    Fix init script

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ elif [ "$DOMAIN_NAME" = "-h" ] || [ "$DOMAIN_NAME" = "--help" ]; then
     echo "$HELP"
     exit 0
 fi
-DOMAIN_DIRECTORY=$(echo "$DOMAIN_NAME" | sed "s/\./-/g")
+APPNAME="laravel"
 
 SSH_USER=$2
 if [ -z "$SSH_USER" ]; then
@@ -39,66 +39,46 @@ fi
 # Generate a new SSH key for the same user for use as a deployment key.
 # This is necessary because the deployment actions need the same access as SSH_USER.
 mkdir -p ~/.ssh
-DEPLOY_KEY_PATH="~/.ssh/${SSH_USER}-${DOMAIN_DIRECTORY}"
-FIND_DEPLOY_KEY=$(find ~/.ssh -maxdepth 1 -name "${SSH_USER}-${DOMAIN_DIRECTORY}" -print -quit)
+DEPLOY_KEY_PATH="~/.ssh/${SSH_USER}-${APPNAME}"
+FIND_DEPLOY_KEY=$(find ~/.ssh -maxdepth 1 -name "${SSH_USER}-${APPNAME}" -print -quit)
 # If the deploy key already exists, don't generate a new one.
 if [ -z "$FIND_DEPLOY_KEY" ]; then
     cd ~/.ssh
-    ssh-keygen -t rsa -b 4096 -C "$SSH_USER@$DOMAIN_NAME" -f "${SSH_USER}-${DOMAIN_DIRECTORY}"
+    ssh-keygen -t rsa -b 4096 -C "$SSH_USER@$DOMAIN_NAME" -f "${SSH_USER}-${APPNAME}"
     cd "$CURRENT_DIRECTORY"
 fi
+echo "Deploy key location: $DEPLOY_KEY_PATH"
 
-# Upload files to the server needed to initialize the instance.
-ssh -i "$SSH_KEY" "$SSH_USER@$DOMAIN_NAME" "mkdir -p /home/bitnami/stack/nginx/html/$DOMAIN_DIRECTORY"
-APP_FOUND=$(ssh -i "$SSH_KEY" "$SSH_USER@$DOMAIN_NAME" "find /home/bitnami/stack/nginx/html/$DOMAIN_DIRECTORY -maxdepth 1 -name 'app.zip' -print -quit")
-if [ -z "$APP_FOUND" ]; then
-    scp -i "$SSH_KEY" app.zip "$SSH_USER@$DOMAIN_NAME:/home/bitnami/stack/nginx/html/$DOMAIN_DIRECTORY/app.zip"
-fi
-SSH_KEY_FOUND=$(ssh -i "$SSH_KEY" "$SSH_USER@$DOMAIN_NAME" "find ~/.ssh -maxdepth 1 -name '${SSH_USER}-${DOMAIN_DIRECTORY}.pub' -print -quit")
+# Upload deployment public key.
+SSH_KEY_FOUND=$(ssh -i "$SSH_KEY" "$SSH_USER@$DOMAIN_NAME" "find ~/.ssh -maxdepth 1 -name '${SSH_USER}-${APPNAME}.pub' -print -quit")
 if [ -z "$SSH_KEY_FOUND" ]; then
-    scp -i "$SSH_KEY" "$HOME/.ssh/${SSH_USER}-${DOMAIN_DIRECTORY}.pub" "$SSH_USER@$DOMAIN_NAME:/home/bitnami/.ssh/${SSH_USER}-${DOMAIN_DIRECTORY}.pub"
+    scp -i "$SSH_KEY" "$HOME/.ssh/${SSH_USER}-${APPNAME}.pub" "$SSH_USER@$DOMAIN_NAME:/home/bitnami/.ssh/${SSH_USER}-${APPNAME}.pub"
 fi
+# Upload application files.
+ssh -i "$SSH_KEY" "$SSH_USER@$DOMAIN_NAME" "mkdir -p /opt/bitnami/nginx/html/$APPNAME"
 
 read -r -d '' SSH_COMMAND << EOM
 DEPLOY_KEY_INCLUDED=\$(cat ~/.ssh/authorized_keys | grep '$SSH_USER@$DOMAIN_NAME')
 if [ -z "\$DEPLOY_KEY_INCLUDED" ]; then
-    echo "Key not found. Adding to authorized_keys."
-    cat /home/bitnami/.ssh/${SSH_USER}-${DOMAIN_DIRECTORY}.pub >> /home/bitnami/.ssh/authorized_keys
+    cat /home/bitnami/.ssh/${SSH_USER}-${APPNAME}.pub >> /home/bitnami/.ssh/authorized_keys
 fi
-cd /home/bitnami/stack/nginx/html/$DOMAIN_DIRECTORY/
-if [ ! -e artisan ]; then
-    unzip app.zip
-fi
-cd /home/bitnami/stack/nginx/conf/server_blocks/
-if [ -e $DOMAIN_DIRECTORY-http-server-block.conf ]; then
-    rm $DOMAIN_DIRECTORY-http-server-block.conf
-fi
-cp /home/bitnami/stack/nginx/html/$DOMAIN_DIRECTORY/nginx/default.conf $DOMAIN_DIRECTORY-http-server-block.conf
-sed -i "s/example.com/$DOMAIN_NAME/g" $DOMAIN_DIRECTORY-http-server-block.conf
-if [ -f $DOMAIN_DIRECTORY-https-server-block.conf ]; then
-    rm $DOMAIN_DIRECTORY-https-server-block.conf
-fi
-cp /home/bitnami/stack/nginx/html/$DOMAIN_DIRECTORY/nginx/https.conf $DOMAIN_DIRECTORY-https-server-block.conf
-sed -i "s/example.com/$DOMAIN_NAME/g" $DOMAIN_DIRECTORY-https-server-block.conf
-cd /home/bitnami/stack/nginx/html/$DOMAIN_DIRECTORY/
-sudo chown -R bitnami:daemon .
-sudo chmod +x update.sh
-sudo chmod -R 777 storage/framework/
-if [ ! -f .env ]; then
-    cp .env.example .env
-fi
-chmod +x artisan
-php artisan key:generate
+cd /opt/bitnami/nginx/conf/server_blocks
+cp sample-server-block.conf.disabled $APPNAME-server-block.conf
+cp sample-https-server-block.conf.disabled $APPNAME-https-server-block.conf
 sudo /opt/bitnami/ctlscript.sh restart nginx
+cd /opt/bitnami/nginx/html
+sudo chown -R bitnami:daemon $APPNAME
 EOM
 
 ssh -i "$SSH_KEY" "$SSH_USER@$DOMAIN_NAME" "$SSH_COMMAND"
 
 read -r -d '' NEXT_STEPS << EOM
 Next steps:
-1. Log in via SSH and run this command to generate your SSL certificate: sudo /opt/bitnami/bncert-tool
+1. Log in via SSH and run this command to generate your SSL certificate:
+sudo /opt/bitnami/bncert-tool
 2. Add the private SSH key to the GitHub repository as a deployment key. Location: $DEPLOY_KEY_PATH
-3. Update the .env file with the correct database credentials, APP_URL, and other values.
+3. Point your custom domain name to this server's static IP address.
+4. Run the deploy.sh script to deploy the application.
 EOM
 
 echo "$NEXT_STEPS"
