fbcab5e04940d622a4e3955827c8c9b76b47d9e5
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-12-23 22:25:44 -0600

    Modify script to accept parameters

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,28 @@
 read -r -d '' HELP << EOM
 This script is used to deploy a Laravel application to an AWS Lightsail instance (NGINX).
 Usage:
-    bash deploy.sh <domain-name>
+    bash deploy.sh <ssh-user=bitnami> <domain-name> <deploy-key-path=\$HOME/.ssh/{ssh-user}-{app-name}> <app-name=laravel>
 EOM
 
-APPNAME="laravel"
-SSH_USER="bitnami"
-DOMAIN_NAME=$1
+SSH_USER=$1
+DOMAIN_NAME=$2
+DEPLOY_KEY_PATH=$3
+APPNAME=$4
+if [ -z "$SSH_USER" ]; then
+    SSH_USER="bitnami"
+fi
 if [ -z "$DOMAIN_NAME" ]; then
     echo "$HELP"
     exit 1
 fi
-
-DEPLOY_KEY_PATH="$HOME/.ssh/${SSH_USER}-${APPNAME}"
+if [ -z "$DEPLOY_KEY_PATH" ]; then
+    DEPLOY_KEY_PATH="$HOME/.ssh/${SSH_USER}-${APPNAME}"
+    if [ ! -f "$DEPLOY_KEY_PATH" ]; then
+        echo "Could not find deploy key at $DEPLOY_KEY_PATH"
+        echo "$HELP"
+        exit 1
+    fi
+fi
 
 # Upload application files.
 ssh -i "$DEPLOY_KEY_PATH" "$SSH_USER@$DOMAIN_NAME" "mkdir -p /opt/bitnami/nginx/html/$APPNAME"
@@ -27,7 +37,6 @@ sudo chmod +x artisan
 sudo chown -R bitnami:daemon .
 sudo chmod -R 777 storage/framework/
 sudo chmod +x update.sh
-./update.sh
 EOM
 
 ssh -i "$SSH_KEY" "$SSH_USER@$DOMAIN_NAME" "$SSH_COMMAND"
