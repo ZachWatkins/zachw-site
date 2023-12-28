6d37cdd8537d6c5c95732fbc53873a1352d75af1
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-12-27 22:13:16 -0600

    Merge selected customizations from my laravel-template repository

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,20 @@ return [
             'throw' => false,
         ],
 
+        'users' => [
+            'driver' => 'local',
+            'root' => storage_path('app/user'),
+            'throw' => false,
+        ],
+
+        'users-public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/user'),
+            'url' => env('APP_URL').'/storage/user',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
